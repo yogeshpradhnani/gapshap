7a7a202d9702c3d2325fb0b76adade9319fc635c
yogeh kumar pradhnani <yogeshpradhnani99@gmail.com>
2024-10-14 00:12:15 +0530

    ho gya

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ io.on('connection', socket => {
     socket.on('new-user-joined', name => {
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
+        console.log(name);
+        
 
     })
 
